ea04da0b611821980fed427ee3ee662904be3076
SojitraJenil <jenilsojitra4@gmail.com>
2024-08-25 20:41:13 +0530

    fix

--- component/record/Record.tsx
+++ component/record/Record.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import TransactionFormModal from "component/TransactionFormModal/TransactionFormModal";
 import TransactionItemNew from "component/TransactionItem/TransactionItemNew/TransactionItemNew";
 import moment from "moment";
