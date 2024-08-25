3984f7a3487ad7cd1046001932f26240fc1dbcd7
SojitraJenil <jenilsojitra4@gmail.com>
2024-08-25 20:24:03 +0530

    fix

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,21 +1,21 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useEffect, useState } from "react";
 import Login from "../../component/login/Login";
-import { useRouter } from "next/router";
-import Cookies from "universal-cookie";
+// import { useRouter } from "next/router";
+// import Cookies from "universal-cookie";
 
 export default function index() {
-  const cookies = new Cookies();
-  const router = useRouter();
+  // const cookies = new Cookies();
+  // const router = useRouter();
 
-  useEffect(() => {
-    const authToken = cookies.get("token");
-    if (authToken) {
-      router.push("/landing");
-    } else {
-      router.push("/login");
-    }
-  }, []);
+  // useEffect(() => {
+  //   const authToken = cookies.get("token");
+  //   if (authToken) {
+  //     router.push("/landing");
+  //   } else {
+  //     router.push("/login");
+  //   }
+  // }, []);
 
   return (
     <div>
