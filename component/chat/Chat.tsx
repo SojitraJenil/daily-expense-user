090bf75101b5955bd2acd6c22698d06398d693b4
SojitraJenil <jenilsojitra4@gmail.com>
2024-09-04 11:22:19 +0530

    socket replace firebase

--- component/chat/Chat.tsx
+++ component/chat/Chat.tsx
@@ -2,17 +2,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { IoSend } from "react-icons/io5";
-import { Box, CircularProgress } from "@mui/material";
+import { collection, addDoc, onSnapshot } from "firebase/firestore";
+import { db } from "../../firebase";
+import WestIcon from "@mui/icons-material/West";
 import moment from "moment";
+import { Box, CircularProgress } from "@mui/material";
 import Cookies from "universal-cookie";
 import { useAtom } from "jotai";
 import { NavigateNameAtom } from "atom/atom";
-import io from "socket.io-client";
 import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 import useHome from "context/HomeContext";
-import { showChatMessage } from "API/api";
-import WestIcon from "@mui/icons-material/West";
 
 const COMMON_ROOM_ID = "ExpenseAllUserChat";
 
@@ -20,75 +20,68 @@ interface Message {
   id: string;
   text: string;
   createdAt: number;
-  author: string;
+  user: string;
+  MobileNumber: string;
   mobileNo: string;
-  _id: string;
-  message: string;
   room: string;
 }
 
 const Chat = () => {
-  const { userProfile, mobileNumber } = useHome();
+  const { userProfile } = useHome();
   const [messages, setMessages] = useState<Message[]>([]);
   const [newMessage, setNewMessage] = useState("");
   const [typing, setTyping] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
-  const messagesEndRef = useRef<HTMLDivElement>(null);
+  const messagesRef = collection(db, "Message");
+  const cookie = new Cookies();
+  const MobileNumber = cookie.get("mobileNumber");
+  const containRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const socket = io("https://daily-expense-api.onrender.com", {
-    transports: ["websocket"],
-  });
-
   useEffect(() => {
-    const fetchMessages = async () => {
-      try {
-        const response = await showChatMessage();
-        if (response) {
-          setMessages(response);
-        } else {
-          console.error("Failed to fetch messages");
-        }
-      } catch (error) {
-        console.error("Error fetching messages:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchMessages();
-
-    socket.emit("join_room", COMMON_ROOM_ID);
-
-    socket.on("receive_message", (msg: Message) => {
-      setMessages((prevMessages) => [...prevMessages, msg]);
+    const unsubscribe = onSnapshot(messagesRef, (snapshot) => {
+      const messages = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      })) as Message[];
+      const sortedMessages = messages.sort((a, b) => a.createdAt - b.createdAt);
+      setLoading(false);
+      setMessages(sortedMessages);
     });
-
-    return () => {
-      socket.off("receive_message");
-    };
+    return () => unsubscribe();
   }, []);
 
   useEffect(() => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    if (containRef.current) {
+      containRef.current.scrollTop = containRef.current.scrollHeight;
+    }
   }, [messages]);
 
-  const sendMessage = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (newMessage.trim()) {
-      socket.emit("send_message", {
-        author: userProfile?.name || "User",
-        message: newMessage,
-        room: COMMON_ROOM_ID,
-        createdAt: new Date().getTime(),
-        mobileNo: mobileNumber,
-      });
-      setNewMessage("");
-    } else {
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  async function sendMessage() {
+    if (newMessage.trim() === "") {
       setError("Enter the text!");
+      return;
     }
-  };
+
+    setNewMessage("");
+    try {
+      await addDoc(messagesRef, {
+        text: newMessage,
+        mobileNo: MobileNumber,
+        createdAt: new Date().getTime(),
+        user: userProfile?.name,
+        room: COMMON_ROOM_ID,
+      });
+    } catch (error) {
+      console.error("Error sending message: ", error);
+      setError("Failed to send message.");
+    }
+  }
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNewMessage(event.target.value);
@@ -100,32 +93,34 @@ const Chat = () => {
     return () => clearTimeout(typingTimeout);
   }, [newMessage]);
 
-  const [, setIsNavigate] = useAtom(NavigateNameAtom);
+  const [, setIsNavigate] = useAtom(NavigateNameAtom); // Use atom to manage navigation state
 
   const handleNavigateHome = () => {
     setIsNavigate("Home");
   };
-
   return (
     <div className="flex flex-col h-screen bg-gray-100 pb-16">
       {/* Header */}
       <div className="flex items-center p-2 border-b border-black bg-red-600 text-white fixed top-0 left-0 w-full z-50">
-        <WestIcon className="w-8 h-8 ml-2" onClick={handleNavigateHome} />
+        <div className="" onClick={handleNavigateHome}>
+          <WestIcon className="w-8 h-8 ml-2" />
+        </div>
         <FaUserCircle className="w-8 h-10 ml-2" />
         <div className="font-semibold text-center flex-grow">
           <p className="text-md text-start ps-2 mt-0">
             {loading ? "Loading..." : "Chat Room".toUpperCase()}
           </p>
           <p className="text-SM text-start ps-2">
-            {typing && (
-              <span className="text-sm">{userProfile?.name} Typing...</span>
-            )}
+            {typing && <p className="text-sm">{userProfile?.name} Typing...</p>}
           </p>
         </div>
       </div>
 
       {/* Messages Container */}
-      <div className="flex-grow overflow-y-auto px-2 pt-5 bg-gray-100">
+      <div
+        className="flex-grow overflow-y-auto px-2 pt-5 bg-gray-100"
+        ref={containRef}
+      >
         <div className="space-y-2">
           {loading ? (
             <Box className="flex justify-center items-center h-full">
@@ -147,26 +142,26 @@ const Chat = () => {
                 )}
                 <div
                   className={`flex ${
-                    mobileNumber === data._id ? "justify-end" : "justify-start"
+                    MobileNumber === data.mobileNo
+                      ? "justify-end"
+                      : "justify-start"
                   }`}
                 >
-                  {mobileNumber !== data._id && (
+                  {MobileNumber !== data.mobileNo && (
                     <FaUserCircle className="w-5 h-8 mr-2" />
                   )}
                   <div
                     className={`p-2 max-w-[300px] rounded-lg ${
-                      mobileNumber === data._id
+                      MobileNumber === data.mobileNo
                         ? "bg-red-600 text-white"
                         : "bg-white text-black"
                     } text-dark`}
                   >
                     <div className="flex flex-col">
-                      {mobileNumber !== data.mobileNo && (
-                        <span className="font-bold">{data.author}:</span>
+                      {MobileNumber !== data.mobileNo && (
+                        <span className="font-bold">{data.user}:</span>
                       )}
-                      <span className="whitespace-pre-wrap">
-                        {data.message}
-                      </span>
+                      <span className="whitespace-pre-wrap">{data.text}</span>
                       <div className="text-xs">
                         {moment(data.createdAt).format("hh:mm A")}
                       </div>
@@ -176,7 +171,6 @@ const Chat = () => {
               </div>
             ))
           )}
-          <div ref={messagesEndRef} />
         </div>
       </div>
 
@@ -185,14 +179,14 @@ const Chat = () => {
         <input
           ref={inputRef}
           type="text"
-          onChange={handleInputChange}
           value={newMessage}
+          onChange={handleInputChange}
           placeholder="Type a message..."
           className="w-full border rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#b54949] focus:border-transparent"
           onKeyPress={(e) => {
             if (e.key === "Enter") {
               e.preventDefault();
-              sendMessage(e);
+              sendMessage();
             }
           }}
         />
@@ -205,6 +199,7 @@ const Chat = () => {
         </button>
       </div>
 
+      {/* Error Message */}
       {error && <p className="text-red-600 p-2">{error}</p>}
     </div>
   );
